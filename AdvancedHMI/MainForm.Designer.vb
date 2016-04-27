6628ab2cf82650e6db87a53033e787fe147ef0fd
unknown <bgreer@b-greer.metal-matic.int>
2016-04-27 11:05:33 -0500

    Initial Commit

---
+++ AdvancedHMI/MainForm.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    '   <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    ' <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.QuickStartLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Meter1 = New AdvancedHMIControls.Meter()
+        Me.EthernetIPforCLXCom1 = New AdvancedHMIDrivers.EthernetIPforCLXCom(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(12, 521)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 32)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "For Development Source Code Visit" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://www.advancedhmi.com"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'QuickStartLabel
+        '
+        Me.QuickStartLabel.AutoSize = True
+        Me.QuickStartLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Bold)
+        Me.QuickStartLabel.ForeColor = System.Drawing.Color.White
+        Me.QuickStartLabel.Location = New System.Drawing.Point(12, 9)
+        Me.QuickStartLabel.Name = "QuickStartLabel"
+        Me.QuickStartLabel.Size = New System.Drawing.Size(273, 104)
+        Me.QuickStartLabel.TabIndex = 38
+        Me.QuickStartLabel.Text = resources.GetString("QuickStartLabel.Text")
+        Me.QuickStartLabel.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Location = New System.Drawing.Point(450, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(322, 47)
+        Me.PictureBox1.TabIndex = 42
+        Me.PictureBox1.TabStop = False
+        '
+        'Meter1
+        '
+        Me.Meter1.CommComponent = Me.EthernetIPforCLXCom1
+        Me.Meter1.HighlightColor = System.Drawing.Color.Red
+        Me.Meter1.Location = New System.Drawing.Point(311, 174)
+        Me.Meter1.MaxValue = New Decimal(New Integer() {100, 0, 0, 0})
+        Me.Meter1.MinValue = New Decimal(New Integer() {0, 0, 0, 0})
+        Me.Meter1.Name = "Meter1"
+        Me.Meter1.NumericFormat = Nothing
+        Me.Meter1.PLCAddressText = ""
+        Me.Meter1.PLCAddressValue = ""
+        Me.Meter1.PLCAddressVisible = ""
+        Me.Meter1.Size = New System.Drawing.Size(198, 169)
+        Me.Meter1.TabIndex = 43
+        Me.Meter1.Text = "Meter1"
+        Me.Meter1.Value = 0.0R
+        Me.Meter1.ValueScaleFactor = New Decimal(New Integer() {1, 0, 0, 0})
+        '
+        'EthernetIPforCLXCom1
+        '
+        Me.EthernetIPforCLXCom1.CIPConnectionSize = 508
+        Me.EthernetIPforCLXCom1.DisableMultiServiceRequest = False
+        Me.EthernetIPforCLXCom1.DisableSubscriptions = False
+        Me.EthernetIPforCLXCom1.IPAddress = "192.168.0.10"
+        Me.EthernetIPforCLXCom1.PollRateOverride = 500
+        Me.EthernetIPforCLXCom1.Port = 44818
+        Me.EthernetIPforCLXCom1.ProcessorSlot = 0
+        '
+        'MainForm
+        '
+        Me.AutoScroll = True
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(784, 562)
+        Me.Controls.Add(Me.Meter1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.QuickStartLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ForeColor = System.Drawing.Color.White
+        Me.KeyPreview = True
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "AdvancedHMI v3.99d"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents QuickStartLabel As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Meter1 As AdvancedHMIControls.Meter
+    Friend WithEvents EthernetIPforCLXCom1 As AdvancedHMIDrivers.EthernetIPforCLXCom
+End Class
---
+++ AdvancedHMI/MainForm.vb
@@ -0,0 +1,45 @@
+﻿Public Class MainForm
+    '*******************************************************************************
+    '* Stop polling when the form is not visible in order to reduce communications
+    '* Copy this section of code to every new form created
+    '*******************************************************************************
+
+    Dim stateTimer As System.Threading.Timer
+
+
+    Private NotFirstShow As Boolean
+    Private Sub Form_VisibleChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.VisibleChanged
+        '* Do not start comms on first show in case it was set to disable in design mode
+        If NotFirstShow Then
+            AdvancedHMIDrivers.Utilities.StopComsOnHidden(components, Me)
+        Else
+            NotFirstShow = True
+        End If
+    End Sub
+
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        stateTimer = New System.Threading.Timer(AddressOf updatemeter, vbNull, 1000, 250)
+
+
+
+
+     
+
+
+    End Sub
+
+
+    Public Sub updatemeter(ByVal StateObj As Object)
+
+        ' Initialize the random-number generator.
+        Randomize()
+        ' Generate random value between 1 and 6. 
+        Dim value As Integer = CInt(Int((99 * Rnd()) + 1))
+        ' Dim meterValue As Double;
+        Meter1.Value = Double.Parse(value.ToString())
+
+    End Sub
+
+End Class
