6112cbd2b514291398662105895ff9e0a00ba0cc
unknown <bgreer@b-greer.metal-matic.int>
2016-04-29 05:10:48 -0500

    Changed Assy Name.

--- AdvancedHMI/MainForm.Designer.vb
+++ AdvancedHMI/MainForm.Designer.vb
@@ -24,25 +24,11 @@ Partial Class MainForm
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.QuickStartLabel = New System.Windows.Forms.Label()
         Me.Meter1 = New AdvancedHMIControls.Meter()
         Me.EthernetIPforCLXCom1 = New AdvancedHMIDrivers.EthernetIPforCLXCom(Me.components)
         Me.Label2 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
-        '
-        'Label1
-        '
-        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
-        Me.Label1.BackColor = System.Drawing.Color.Transparent
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!)
-        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
-        Me.Label1.Location = New System.Drawing.Point(12, 521)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(194, 32)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "For Development Source Code Visit" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://www.advancedhmi.com"
-        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
         '
         'QuickStartLabel
         '
@@ -60,7 +46,7 @@ Partial Class MainForm
         '
         Me.Meter1.CommComponent = Me.EthernetIPforCLXCom1
         Me.Meter1.HighlightColor = System.Drawing.Color.Red
-        Me.Meter1.Location = New System.Drawing.Point(311, 174)
+        Me.Meter1.Location = New System.Drawing.Point(297, 170)
         Me.Meter1.MaxValue = New Decimal(New Integer() {100, 0, 0, 0})
         Me.Meter1.MinValue = New Decimal(New Integer() {0, 0, 0, 0})
         Me.Meter1.Name = "Meter1"
@@ -68,7 +54,7 @@ Partial Class MainForm
         Me.Meter1.PLCAddressText = ""
         Me.Meter1.PLCAddressValue = ""
         Me.Meter1.PLCAddressVisible = ""
-        Me.Meter1.Size = New System.Drawing.Size(198, 169)
+        Me.Meter1.Size = New System.Drawing.Size(269, 230)
         Me.Meter1.TabIndex = 43
         Me.Meter1.Text = "Meter1"
         Me.Meter1.Value = 0.0R
@@ -90,9 +76,9 @@ Partial Class MainForm
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.Location = New System.Drawing.Point(291, 102)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(237, 31)
+        Me.Label2.Size = New System.Drawing.Size(300, 31)
         Me.Label2.TabIndex = 44
-        Me.Label2.Text = "STRESS METER"
+        Me.Label2.Text = "MMI STRESS METER"
         '
         'MainForm
         '
@@ -102,7 +88,6 @@ Partial Class MainForm
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Meter1)
         Me.Controls.Add(Me.QuickStartLabel)
-        Me.Controls.Add(Me.Label1)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ForeColor = System.Drawing.Color.White
         Me.KeyPreview = True
@@ -113,7 +98,6 @@ Partial Class MainForm
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As System.Windows.Forms.Label
     Friend WithEvents QuickStartLabel As System.Windows.Forms.Label
     Friend WithEvents Meter1 As AdvancedHMIControls.Meter
     Friend WithEvents EthernetIPforCLXCom1 As AdvancedHMIDrivers.EthernetIPforCLXCom
